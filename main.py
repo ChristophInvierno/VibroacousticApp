7e911d5922d7d26b036c7e2bcfa503d42831863a
ravil <ravil.mobile.com@gmail.com>
2017-06-01 19:30:35 +0200

    Basic layout was created

--- main.py
+++ main.py
@@ -2,9 +2,10 @@
 from bokeh.plotting import figure
 from bokeh.layouts import column, row, Spacer
 from bokeh.io import curdoc
-from bokeh.models import Slider, Button, Div, Arrow, OpenHead, Label, Plot
+from bokeh.models import Div, Label, Plot
 from bokeh.plotting import figure, output_file, show
 from bokeh.models.glyphs import Text
+from bokeh.models.widgets import Button, RadioButtonGroup, Select, Slider
 
 # Link third-party python libraries
 from math import cos, sin, radians, sqrt, pi, atan2
@@ -14,15 +15,52 @@ from LatexSupport import LatexLabel
 
 
 def main():
-    fig = figure( title = "",
-                  tools = "",
-                  x_range = (-7, 7),
-                  y_range = (-1, 30),
-                  width = 350,
-                  height = 500 )
+
+    # Create all Widgets
+    Input = figure( title = "",
+                    tools = "",
+                    width = 500,
+                    height = 500 )
 
 
-    curdoc().add_root( column( fig ) )
+    Graph = figure( title = "",
+                    tools = "",
+                    x_range = (-7, 7),
+                    y_range = (-1, 30),
+                    width = 500,
+                    height = 500 )
+
+
+    ApplyButton = Button( label = "Apply",
+                          button_type = "success",
+                          width = 100)
+
+
+    PrintReport = Button( label = "Print Report",
+                          button_type = "success",
+                          width = 100 )
+
+    ModeRadioButtons = RadioButtonGroup( labels = [ "Homogeneous mode",
+                                                    "Non-Homogeneous mode" ],
+                                     width = 500,
+                                     active = 0 )
+
+
+    GraphRadioButtons = RadioButtonGroup( labels = [ "Option 1",
+                                                     "Option 2",
+                                                     "Option 3",
+                                                     "Option 4",
+                                                     "Option 5"],
+                                     width = 500,
+                                     active = 0 )
+
+
+    Buttons = row( Spacer(width=175), column( ApplyButton, PrintReport ) )
+
+    RightSide = column( ModeRadioButtons, Input, Buttons )
+    LeftSide = column( GraphRadioButtons, Graph )
+    curdoc().add_root( column( Spacer( height = 20 ),
+                               row( RightSide, LeftSide ) ) )
 
 
 main()
